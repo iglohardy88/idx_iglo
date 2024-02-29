53dca24fe5a452461fcb505718e645091abb04e5
sylvia <avia3010@email.com>
2024-02-29 21:25:42 +0700

    a

--- Test Suites/TS_Google.ts
+++ Test Suites/TS_Google.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>9619f8b2-b31e-419b-9ad8-de684b950966</testSuiteGuid>
+   <testCaseLink>
+      <guid>b0c1bdf1-2b90-4db5-a732-b795e5245451</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/google</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
